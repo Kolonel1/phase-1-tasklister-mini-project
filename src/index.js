7202bffd7c677ef7a63dd0e478da10940f31771c
Kolonel1 <victor.munyao@student.moringaschool.com>
2022-08-26 01:38:29 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form =document.querySelector('form')
+  form.addEventListener('submit',function(e){
+    e.preventDefault()
+   handler(e.target.querySelector('#new-task-description').value)
+   form.reset()
+  })
 });
+
+
+function handler(value){
+  let li = document.createElement('li')
+  li.textContent = value
+  console.log(li)
+  let btn = document.createElement('button')
+  btn.textContent = 'x'
+  btn.addEventListener('click',deleteInput)
+  li.appendChild(btn)
+
+  let ul = document.getElementById('tasks')
+  console.log(ul)
+  ul.appendChild(li)
+}
+
+function deleteInput(e){
+e.target.parentNode.remove()
+}
+
+//now i see the benefits of appending as child 
